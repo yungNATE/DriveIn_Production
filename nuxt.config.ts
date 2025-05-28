efe39e06e86b25f327b1da2eb93025f6a52d4f7a
yungNATE <nathan.martinigol@gmail.com>
2025-05-28 23:22:53 +0200

    Chnagement du preset de rendu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  modules: ["@nuxt/content"],
+  ssr: true,
+  nitro: {
+    preset: "netlify",
+  },
+});
