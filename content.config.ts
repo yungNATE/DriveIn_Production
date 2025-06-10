bb10e074e94a674ee88921631ba757b4a3b83a35
yungNATE <nathan.martinigol@gmail.com>
2025-06-10 21:39:30 +0200

    Création d'un type de contenu, à voir si c'est utile à l'avenir

---
+++ content.config.ts
@@ -0,0 +1,18 @@
+// import { defineCollection, defineContentConfig, z } from "@nuxt/content";
+
+// export default defineContentConfig({
+//   collections: {
+//     projets: defineCollection({
+//       source: "projets/*.md",
+//       type: "page",
+//       schema: z.object({
+//         title: z.string(),
+//         presentation: z.string(),
+//         types: z.array(z.string()),
+//         video: z.string().url().optional(),
+//         client: z.array(z.string()),
+//         tags: z.array(z.string()),
+//       }),
+//     }),
+//   },
+// });
