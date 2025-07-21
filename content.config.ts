13d19fd3381bd83d27765a6a4a93f056df280434
yungNATE <nathan.martinigol@gmail.com>
2025-07-21 23:01:38 +0200

    Update schemas

--- content.config.ts
+++ content.config.ts
@@ -1,18 +1,25 @@
-// import { defineCollection, defineContentConfig, z } from "@nuxt/content";
+import { defineCollection, defineContentConfig, z } from "@nuxt/content";
 
-// export default defineContentConfig({
-//   collections: {
-//     projets: defineCollection({
-//       source: "projets/*.md",
-//       type: "page",
-//       schema: z.object({
-//         title: z.string(),
-//         presentation: z.string(),
-//         types: z.array(z.string()),
-//         video: z.string().url().optional(),
-//         client: z.array(z.string()),
-//         tags: z.array(z.string()),
-//       }),
-//     }),
-//   },
-// });
+export default defineContentConfig({
+  collections: {
+    projects: defineCollection({
+      source: "projects/*.md",
+      type: "data",
+      schema: z.object({
+        tags: z.array(z.string()),
+        image: z.string(),
+        presentation: z.string(),
+        highlighted: z.string().optional(),
+      }),
+    }),
+    partners: defineCollection({
+      source: "partners/*.md",
+      type: "data",
+      schema: z.object({
+        name: z.string(),
+        logo: z.string(),
+        description: z.string(),
+      }),
+    }),
+  },
+});
