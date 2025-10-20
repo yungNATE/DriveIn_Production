e9c6fd1525da486e97020b92bf64d10b582c5c3e
yungNATE <nathan.martinigol@gmail.com>
2025-10-20 20:49:06 +0200

    ajout de nuxt images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: false },
-  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/scripts"],
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/scripts", "@nuxt/image"],
   ssr: false,
   nitro: {
     preset: "static",
