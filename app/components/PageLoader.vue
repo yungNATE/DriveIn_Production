4cd721a1d0115d4678b4c4186d5f6429f81225ee
yungNATE <nathan.martinigol@gmail.com>
2026-09-08 15:32:33 +0200

    Correction page loader

--- app/components/PageLoader.vue
+++ app/components/PageLoader.vue
@@ -81,8 +81,8 @@ router.afterEach(() => {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: black;
   z-index: 9999;
 
